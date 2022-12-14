1761173b59e3a2331a3d4d0ce479e1cda40b23ad
Nanda <iamrajgopal@gmail.com>
2022-12-14 15:30:12 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>This is from local repository</h3>
-     <h3>This is from git repository <h3>
+     <h3>This is from git repository </h3>
+      
     </div>
   );
 }
